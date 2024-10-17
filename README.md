36a97b1569e0793dc7e3b2abde1a5ee42d2a3172
wailsb <wail.saribey@gmail.com>
2024-10-17 12:08:16 +0100

    added some files

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ a simple os with a bootloader programmed with assembly
 ## if you modify the assembly the command you'll need to regenerate the iso img file
 
 ```console
-$cp bootloader.bin iso/
-$genisoimage -o myimage.iso -b bootloader.bin -no-emul-boot -boot-load-size 4 -boot-info-table iso/
+$chmod +755 Regen.sh
+$./Regen.sh
 ```
 ### run using :
 ```console
--- bootloader.asm
+++ bootloader.asm
@@ -1,4 +1,37 @@
 loop:
+    mov ah,0xe
+    mov al,"H"
+    int 0x10
+    mov ah,0xe
+    mov al,"E"
+    int 0x10
+    mov ah,0xe
+    mov al,"L"
+    int 0x10
+    mov ah,0xe
+    mov al,"L"
+    int 0x10
+    mov ah,0xe
+    mov al,"O"
+    int 0x10
+    mov ah,0xe
+    mov al," "
+    int 0x10
+    mov ah,0xe
+    mov al,"W"
+    int 0x10
+    mov ah,0xe
+    mov al,"O"
+    int 0x10
+    mov ah,0xe
+    mov al,"R"
+    int 0x10
+    mov ah,0xe
+    mov al,"L"
+    int 0x10
+    mov ah,0xe
+    mov al,"D"
+    int 0x10
     jmp loop
 times 510-($-$$) db 0
 dw 0x55aa
