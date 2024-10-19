e189b0881acc7a0dc08d7fd07435bb8efda7f124
wailsb <wail.saribey@gmail.com>
2024-10-19 13:25:07 +0100

    randomly testing assembly codes

--- bootloader.asm
+++ bootloader.asm
@@ -1,22 +1,14 @@
 mov ah,0x0e
-start:
-    ; Print "HELLO" using Al regter only
-    mov al, "h"  ; Set AH = 0x0E (teletype function), AL = 'H'
-    int 0x10        ; Print 'H'
-    
-    mov al, "e"  ; Set AL = 'E'
-    int 0x10        ; Print 'E'
-    
-    mov al, "l"  ; Set AL = 'L'
-    int 0x10        ; Print 'L'
-    
-    mov al, "l"  ; Set AL = 'L' again
-    int 0x10        ; Print 'L'
-    
-    mov al, "o"  ; Set AL = 'O'
-    int 0x10        ; Print 'O'
-    
-    jmp start           ; Infinite loop to keep the bootloader running
-
-times 510 - ($ - $$) db 0 ; Fill the rest of the boot sector with zeros
-dw 0xaa55          ; Boot sector signature
+mov bx , the_secret
+mov al,[bx]
+int 0x10
+add bx , 0x7c00
+mov al,[0x7c00]
+int 0x10;
+mov al,[bx]
+int 0x10;
+jmp $
+the_secret :
+    db "X"
+times 510 -( $ - $$ ) db 0
+dw 0xaa55
