3ece2365c772ead7102aa8018fe738ca10228e00
wailsb <wail.saribey@gmail.com>
2024-10-20 13:38:08 +0100

    adding comments

--- bootloader.asm
+++ bootloader.asm
@@ -1,13 +1,18 @@
-booting_message:
-    db "Hello!!......loading bootloader",0
-
-
-
-mov bx,0x7c00
-add bx,booting_message
+;address where our program is loaded
+[org 0x7c00]
+;our code
+mov bx,booting_message
+call print_msg
+mov bx,after
 call print_msg
 jmp $
-
+;message booting string
+booting_message:
+    db "Hello!!......loading bootloader",0
+;message after booting string
+after:
+    db 0x0A,"Bootloader ... loaded",0
+;print message function
 print_msg:
     pusha
     mov ah,0x0e
